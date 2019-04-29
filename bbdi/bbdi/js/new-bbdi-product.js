b5a8bdcca496999c5da768b804953ad6b84da8b0
Lucas Santos <ceo.lucascs@gmail.com>
2019-04-29 15:53:23 -0300

    29/4/2019 - 15:53

---
+++ bbdi/bbdi/js/new-bbdi-product.js
@@ -0,0 +1,81 @@
+var productName = $(".New-BBDI-product .product-info .productName").text().toUpperCase();
+var brandLogo = $(".New-BBDI-product .brand-logo img");
+
+if (productName.indexOf("ACER") !== -1 || productName.indexOf("BB11-AC") !== -1 || productName.indexOf("BB20-AC") !== -1 || productName.indexOf("KB-AC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-acer.jpg"); } 
+else if (productName.indexOf("AMAZON") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-amazon-pc.jpg"); } 
+else if (productName.indexOf("APPLE") !== -1 || productName.indexOf("BB11-AP") !== -1 || productName.indexOf("BB20-AP") !== -1 || productName.indexOf("KB-AP") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-apple.jpg"); } 
+else if (productName.indexOf("ASUS") !== -1 || productName.indexOf("BB11-AS") !== -1 || productName.indexOf("BB20-AS") !== -1 || productName.indexOf("KB-AS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-asus.jpg"); } 
+else if (productName.indexOf("AUO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-auo.jpg"); } 
+else if (productName.indexOf("BENQ") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-benq.jpg"); } 
+else if (productName.indexOf("BLACKBERRY") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-blackberry.jpg"); } 
+else if (productName.indexOf("BOEE") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-boe.jpg"); } 
+else if (productName.indexOf("BUSTER") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-buster.jpg"); } 
+else if (productName.indexOf("CANON") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-canon.jpg"); } 
+else if (productName.indexOf("CASIO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-casio.jpg"); } 
+else if (productName.indexOf("CCE") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-cce.jpg"); } 
+else if (productName.indexOf("CHIMEI") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-chimei.jpg"); } 
+else if (productName.indexOf("CHUNGHWA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-chunghwa.jpg"); } 
+else if (productName.indexOf("CLEVO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-clevo.jpg"); } 
+else if (productName.indexOf("COMPAL") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-compal.jpg"); } 
+else if (productName.indexOf("COMPAQ") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-compaq.jpg"); } 
+else if (productName.indexOf("DELL") !== -1 || productName.indexOf("BB11-DE") !== -1 || productName.indexOf("BB20-DE") !== -1 || productName.indexOf("KB-DE") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-dell.jpg"); } 
+else if (productName.indexOf("DOPOD") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-dopod.jpg"); } 
+else if (productName.indexOf("ECS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-ecs.jpg"); } 
+else if (productName.indexOf("EMACHINES") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-emachines.jpg"); } 
+else if (productName.indexOf("EPSON") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-epson.jpg"); } 
+else if (productName.indexOf("FOSTON") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-foston.jpg"); } 
+else if (productName.indexOf("FUJIFILM") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-fujifilm.jpg"); } 
+else if (productName.indexOf("FUJITSU") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-fujitsu.jpg"); } 
+else if (productName.indexOf("GATEWAY") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-gateway.jpg"); } 
+else if (productName.indexOf("GIGABYTE") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-gigabyte.jpg"); } 
+else if (productName.indexOf("HANDSPRING") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-handspring.jpg"); } 
+else if (productName.indexOf("HANNSTAR") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-hannstar.jpg"); } 
+else if (productName.indexOf("HP") !== -1 || productName.indexOf("BB11-HP") !== -1 || productName.indexOf("BB20-HP") !== -1 || productName.indexOf("KB-HP") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-hp.jpg"); } 
+else if (productName.indexOf("HTC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-htc.jpg"); } 
+else if (productName.indexOf("IBM") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-ibm.jpg"); } 
+else if (productName.indexOf("INFOVISION") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-infovision.jpg"); } 
+else if (productName.indexOf("INNOLUX") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-innolux.jpg"); } 
+else if (productName.indexOf("INTELBRAS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-intelbras.jpg"); } 
+else if (productName.indexOf("ITAUTEC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-itautec.jpg"); } 
+else if (productName.indexOf("JVC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-jvc.jpg"); } 
+else if (productName.indexOf("KENNEX") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-kennex.jpg"); } 
+else if (productName.indexOf("KENWOOD") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-kenwood.jpg"); } 
+else if (productName.indexOf("KINGSTON") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-kingston.jpg"); } 
+else if (productName.indexOf("KODAK") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-kodak.jpg"); } 
+else if (productName.indexOf("KONICA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-konica.jpg"); } 
+else if (productName.indexOf("KYOCERA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-kyocera.jpg"); } 
+else if (productName.indexOf("LENOVO") !== -1 || productName.indexOf("BB11-LE") !== -1 || productName.indexOf("BB20-LE") !== -1 || productName.indexOf("KB-LE") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-lenovo.jpg"); } 
+else if (productName.indexOf("LG") !== -1 || productName.indexOf("BB11-LG") !== -1 || productName.indexOf("BB20-LG") !== -1 || productName.indexOf("KB-LG") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-lg.jpg"); } 
+else if (productName.indexOf("LG-PHILIPS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-lg-philips.jpg"); } 
+else if (productName.indexOf("MEGAWARE") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-megaware.jpg"); } 
+else if (productName.indexOf("MICROBOARD") !== -1 || productName.indexOf("BB11-MI") !== -1 || productName.indexOf("BB20-MI") !== -1 || productName.indexOf("KB-MI") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-microboard.jpg"); } 
+else if (productName.indexOf("MINOLTA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-minolta.jpg"); } 
+else if (productName.indexOf("MITAC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-mitac.jpg"); } 
+else if (productName.indexOf("MOTOROLA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-motorola.jpg"); } 
+else if (productName.indexOf("MSI") !== -1 || productName.indexOf("BB11-MS") !== -1 || productName.indexOf("BB20-MS") !== -1 || productName.indexOf("KB-MS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-msi.jpg"); } 
+else if (productName.indexOf("NEC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-nec.jpg"); } 
+else if (productName.indexOf("NIKON") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-nikon.jpg"); } 
+else if (productName.indexOf("OLYMPUS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-olympus.jpg"); } 
+else if (productName.indexOf("OUTRAS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-outras-marcas.jpg"); } 
+else if (productName.indexOf("PALM") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-palm.jpg"); } 
+else if (productName.indexOf("PANASONIC") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-panasonic.jpg"); } 
+else if (productName.indexOf("PCCHIPS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-pcchips.jpg"); } 
+else if (productName.indexOf("PHILCO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-philco.jpg"); } 
+else if (productName.indexOf("PHILIPS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-philips.jpg"); } 
+else if (productName.indexOf("POSITIVO") !== -1 || productName.indexOf("BB11-PO") !== -1 || productName.indexOf("BB20-PO") !== -1 || productName.indexOf("KB-PO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-positivo.jpg"); } 
+else if (productName.indexOf("QTEK") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-qtek.jpg"); } 
+else if (productName.indexOf("QUANTA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-quanta.jpg"); } 
+else if (productName.indexOf("REFERENCIAS") !== -1 || productName.indexOf("BB11-NA") !== -1 || productName.indexOf("BB20-NA") !== -1 || productName.indexOf("KB-NA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-referencias.jpg"); } 
+else if (productName.indexOf("SAMSUNG") !== -1 || productName.indexOf("BB11-SS") !== -1 || productName.indexOf("BB20-SS") !== -1 || productName.indexOf("KB-SS") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-samsung.jpg"); } 
+else if (productName.indexOf("SANYO") !== -1 || productName.indexOf("BB11-SA") !== -1 || productName.indexOf("BB20-SA") !== -1 || productName.indexOf("KB-SA") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-sanyo.jpg"); } 
+else if (productName.indexOf("SEMP") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-semp-toshiba.jpg"); } 
+else if (productName.indexOf("SHARP") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-sharp.jpg"); } 
+else if (productName.indexOf("SONY") !== -1 || productName.indexOf("BB11-SO") !== -1 || productName.indexOf("BB20-SO") !== -1 || productName.indexOf("KB-SO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-sony.jpg"); } 
+else if (productName.indexOf("TOPPOLY") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-toppoly.jpg"); } 
+else if (productName.indexOf("TOSHIBA") !== -1 || productName.indexOf("BB11-TS") !== -1 || productName.indexOf("BB20-TO") !== -1 || productName.indexOf("KB-TO") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-toshiba.jpg"); } 
+else if (productName.indexOf("TWINHEAD") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-twinhead.jpg"); } 
+else if (productName.indexOf("UNIDEN") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-uniden.jpg"); } 
+else if (productName.indexOf("UNIVERSAL") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-universal.jpg"); } 
+else if (productName.indexOf("UNIWILL") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-uniwill.jpg"); } 
+else if (productName.indexOf("XBOX") !== -1) { $(brandLogo).attr("src", "/arquivos/brand-prod-xbox.jpg"); } 
+else { $(".brand-logo img").attr("src", "/arquivos/brand-prod-referencias.jpg"); }
